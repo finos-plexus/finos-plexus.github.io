7fc8c6173298ec001b6dd82b283794ef1f6d47ed
Georgi Georgiev <georgi.georgiev@tick42.com>
2019-05-16 19:55:04 +0300

    Move the web-trader broker to the demos folder

--- demos/web-trader/start-plexus-broker.bat
+++ demos/web-trader/start-plexus-broker.bat
@@ -1,1 +1,1 @@
-%~dp0/broker/redist/Plexus.Interop.Broker.Redist-win-x86/plexus.exe broker -p 52486 -m plexus-config
+%~dp0/../broker/redist/Plexus.Interop.Broker.Redist-win-x86/plexus.exe broker -p 52486 -m plexus-config
